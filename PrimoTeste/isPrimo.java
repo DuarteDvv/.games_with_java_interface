6be92b19565732183edbbc47f2b52dc102e5508a
Luis Antonio Duarte Sousa <136333571+DuarteDvv@users.noreply.github.com>
2023-12-12 09:57:29 -0300

    teste2

--- PrimoTeste/isPrimo.java
+++ PrimoTeste/isPrimo.java
@@ -1,9 +1,25 @@
+import java.util.Scanner;
+
 package PrimoTeste;
 class Primo{
     public static void main(String[] arg) {
-        int a = 10;
+        Scanner scan = new Scanner(System.in);
 
-        System.out.println(a);
-        
+        System.out.println("Digite um numero");
+        int a = scan.nextInt();
+
+        if(a = 1){
+            System.out.println("Não é primo");
+            return 0;
+        }
+       
+        for(int i = 1; i < a; ++i){
+            if(a%i = 0){
+                System.out.println("Não é primo");
+                return 0;
+            }
+        }
+
+        System.out.println("é primo");
     }
 };

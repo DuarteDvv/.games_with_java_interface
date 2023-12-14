b13f20f97147f74c7bc11bfe0d1cc1ae626ce33f
Luis Antonio Duarte Sousa <136333571+DuarteDvv@users.noreply.github.com>
2023-12-14 10:47:33 -0300

    att

---
+++ Structured_Logic/Abreviador/Abrevia.java
@@ -0,0 +1,20 @@
+package Abreviador;
+
+import java.nio.*;
+import java.util.*;
+
+public class Abrevia{
+    public static void main(String[] a){ //Paulo Jose de Almeida Prado. Abreviatura: P. J. de A. P.
+        Scanner scan = new Scanner();
+        String nome = new String();
+
+        String[] nomeSeparado = nome.split(" ");
+        for(n : nomeSeparado){
+            if(n.lenght() > 2){
+                
+            }
+
+        }
+
+    }
+}
---
+++ Structured_Logic/isSubStr/LookingForSubStr.java
@@ -0,0 +1,20 @@
+package isSubStr;
+
+import java.nio.*;
+import java.util.*;
+
+
+public class LookingForSubStr{
+    publicb static void main(String[] a){
+        Scanner scan = new Scanner();
+        String A = nextLn();
+        String B = nextLn();
+        scan.close();
+
+
+
+        
+
+
+    }
+}

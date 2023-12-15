17de48a06c4138603e2c40c729c9d36760a673f4
DuarteDvv <luisduarte.dev@gmail.com>
2023-12-15 01:03:35 -0300

    Cifra

---
+++ Structured_Logic/CifraCeasar/Cifra.java
@@ -0,0 +1,34 @@
+package CifraCeasar;
+
+import java.util.Scanner;
+
+public class Cifra {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Digite a frase:");
+        String frase = scan.nextLine();
+
+        System.out.println("Digite o valor de K (o deslocamento):");
+        int k = scan.nextInt();
+
+        scan.close();
+        
+        frase = frase.replaceAll(" ", "");
+
+        char[] f = frase.toCharArray();
+
+
+        for (int i = 0; i < f.length; i++) {
+           
+            if (Character.isLetter(f[i])) {
+                f[i] = Character.toLowerCase(f[i]);
+            }
+            f[i] = (char) ((int) f[i] + k);
+        }
+
+        String fraseCodificada = new String(f);
+
+        System.out.println("Frase codificada: " + fraseCodificada);
+    }
+}

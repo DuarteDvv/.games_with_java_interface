784946e4fc061c9237aeabd7ede4287e0f4410ef
Luis Antonio Duarte Sousa <136333571+DuarteDvv@users.noreply.github.com>
2023-12-20 09:30:57 -0300

    Teste

--- Graphic_API/CardMemoryGame/CardGame.java
+++ Graphic_API/CardMemoryGame/CardGame.java
@@ -1,2 +1,43 @@
 package CardMemoryGame;
 
+import java.util.*;
+import javax.swing.*;
+import java.awt.event.*
+
+
+public class CardGame extends Jframe{
+
+
+
+    CardGame(){
+        setTitle("MemoryCard");
+        setSize(500,800);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Jpanel Dificuldade = new Jpanel();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        Jpanel Base = new Jpanel();
+        add(Base);
+
+    }
+
+
+
+    public static void main(String[] args) {
+        new CardGame();
+    }
+}

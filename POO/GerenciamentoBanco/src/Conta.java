64f42796ebaef2f6f23e44b88c72c3d73e6f3436
Luis Antonio Duarte Sousa <luisduarte.dev@gmail.com>
2023-12-29 11:09:29 -0300

    att

--- POO/GerenciamentoBanco/src/Conta.java
+++ POO/GerenciamentoBanco/src/Conta.java
@@ -6,6 +6,8 @@ public class Conta{
 
   private String titular;
 
+  private String agencia;
+
   private int numero;
 
   private double saldo;
@@ -29,37 +31,43 @@ public class Conta{
     titular = tit;
     numero = num;
     saldo = sald;
-    
-    
 
     numeroDeContas++;
     identificador = numeroDeContas;
-    
   }
+  
+  public void transfere(Conta destino, double valor){
+    if(sacar(valor)){
+      destino.depositar(valor);
 
+    }
+    else{
+      System.out.Println("Erro, saldo indisponivel");
+    }
+    
+
+  }
   public boolean sacar (double sacado){
-
     return (saldo - sacado) >= 0 ? (saldo -= sacado) == saldo : false;
-
-
   }
 
 
   public void depositar (double depositado){
-
     saldo += depositado;
-
   }
 
   public double rendimentoMensal (){
-
     return saldo * 0.1;
-
   }
 
 
   public String imprimeDados (){
     return titular + " " + numero + " " + saldo + " " + dataEntrada;
+
+  }
+
+  public String getTipoDeConta(){
+    return "Conta";
 
   }
 

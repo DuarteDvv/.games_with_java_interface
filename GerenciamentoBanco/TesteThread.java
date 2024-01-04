11ccea8156093d844f3f5b909158d9c7c3ed9c64
Luis Antonio Duarte Sousa <luisduarte.dev@gmail.com>
2024-01-04 11:09:46 -0300

    TESTErUNNABLE

--- GerenciamentoBanco/TesteThread.java
+++ GerenciamentoBanco/TesteThread.java
@@ -2,7 +2,7 @@ import java.lang.*
 
 class GerarPdf implements Runnable{
     public void run() {
-        
+
     }
 
 }
@@ -15,6 +15,24 @@ class Barra implements Runnable{
 
 public class TesteThread {
     public static void main(String[] args){
+        Runnable Principal = () -> { // só funciona pq tem apenas um método abstrato definido na interface
+
+        };
+
+        Thread a = new Thread(Principal);
+        a.start();
+
+        // melhor forma 
+
+        Runnable Principal2 = new Runnable() { // classe anonima pode implementar uma interface
+            public void run(){
+
+            }
+
+        };
+
+        Thread b = new Thread(Principal2);
+        b.start();
 
     }
 

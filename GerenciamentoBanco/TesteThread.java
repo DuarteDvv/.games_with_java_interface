a8f4ce65a3895222d2158d9512275bd3f43610cc
Luis Antonio Duarte Sousa <luisduarte.dev@gmail.com>
2024-01-04 10:22:38 -0300

    att

---
+++ GerenciamentoBanco/TesteThread.java
@@ -0,0 +1,22 @@
+import java.lang.*
+
+class GerarPdf implements Runnable{
+    public void run() {
+        
+    }
+
+}
+
+class Barra implements Runnable{
+    public void run(){
+
+    }
+}
+
+public class TesteThread {
+    public static void main(String[] args){
+
+    }
+
+
+}
